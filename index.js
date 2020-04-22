0c90033f186f0ccfd46569a8ec1b6339d0ad159d
arn-foto <38259824+arn-foto@users.noreply.github.com>
2020-04-22 13:44:38 -0700

    initial commit

--- index.js
+++ index.js
@@ -25,14 +25,20 @@ function processFirstItem(stringList, callback) {
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
- *  
+ * 
  * 1. What is the difference between counter1 and counter2?
- *    counter1 has a private variable that can only be accessed by that particular function, but are free to use anywhere within that function. 
- *    counter2 is global and be accessed anywhere.
+ * 
+ * Counter1 uses two functions in order to use counter1 you'd use counterMaker(parameter) to use counter2 you
+ * could simply call counter2() but count needs to be set beforehand.
  * 
  * 2. Which of the two uses a closure? How can you tell?
- *     
+ * 
+ * Counter1 uses a closure it uses a parent element to set the variable, and child function to increment it.
+ * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+ * 
+ * counter2 will be better if you need to manipulate the count variable. Counter1 is better if you need to set
+ * multiple scores at once.
  *
 */
 
@@ -43,54 +49,82 @@ function counterMaker() {
    return count++;
   }
 }
-const counter1 = counterMaker();
 
+const counter1 = counterMaker();
 
 // counter2 code
 let count = 0;
+
 function counter2() {
+  return count++;
+}
 
 
 /* Task 2: inning() 
-
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+// function inning(max) {
+//   return Math.floor(Math.random() * Math.floor(max));
+// }
+// console.log(inning(3));
+// I couldnt figure out how to get this to work below.
 
-    /*Code Here*/
 
+
+function inning(max){
+  let random = Math.random() * 50;
+  // creates variable set to a random value between 0-49
+  if(random >= 35){
+    let score = 2;
+    return score; 
+  }
+  // returns 2 points if random number is over or equal to 35
+  else if(random > 21 && random < 35){
+    let score = 1;
+    return score;
+  }
+  // returns 1 point if random number is between 21 and 35
+  else{
+    let score = 0;
+    return score;
+  }
+  //otherwise no points
 }
 
+
+// console.log(inning());
+
+
 /* Task 3: finalScore()
-
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
-
 For example, 
-
 finalScore(inning, 9) might return: 
 {
   "Home": 11,
   "Away": 5,
 }
-
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(inning, number){
+let home = 0;
+let away = 0;
+let finalScore = {};
+  for(let i = 0; i < number;i++){
+    home += inning(home);
+    away += inning(away);
+    }
+    finalScore.Home = home;
+    finalScore.Away = away;
+    return finalScore;
+  }
 
-  /*Code Here*/
-
-}
-
+// console.log(finalScore(inning, 9));
 /* Task 4: 
-
 Create a function called `scoreboard` that accepts the following parameters: 
-
 (1) Callback function `getInningScore`
 (2) Callback function `inning`
 (2) A number of innings
-
 and returns the score at each pont in the game, like so:
-
 1st inning: awayTeam - homeTeam
 2nd inning: awayTeam - homeTeam
 3rd inning: awayTeam - homeTeam
@@ -100,11 +134,30 @@ and returns the score at each pont in the game, like so:
 7th inning: awayTeam - homeTeam
 8th inning: awayTeam - homeTeam
 9th inning: awayTeam - homeTeam
-
 Final Score: awayTeam - homeTeam */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(inning, number) {
+  let homeFinal = 0;
+  let awayFinal =0;
+  for(let i = 1; i <= number; i++){
+    let homeTeam=inning();
+    let awayTeam=inning();
+    if(i ==  1){
+    console.log(`${i}st inning: ${awayTeam} - ${homeTeam}`);
+    }
+    else if(i ==  2){
+      console.log(`${i}nd inning: ${awayTeam} - ${homeTeam}`);
+      }
+      else if(i ==  3){
+        console.log(`${i}rd inning: ${awayTeam} - ${homeTeam}`);
+        }
+        else{
+          console.log(`${i}th inning: ${awayTeam} - ${homeTeam}`);
+          }
+    homeFinal+=homeTeam; //The += assignment operator can also be used to add (concatenate) strings
+    awayFinal+=awayTeam;
+  }
+  console.log(`Final score: ${awayFinal} - ${homeFinal}`)
 }
 
-
+scoreboard(inning, 9); 
